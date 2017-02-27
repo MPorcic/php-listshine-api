d6a3332dd0d94823a31914778eab45e860088a35
MPorcic <porcicmario@gmail.com>
2017-02-27 18:59:14 +0100

    Update ListShine_Api.php

--- ListShine_Api.php
+++ ListShine_Api.php
@@ -4,7 +4,7 @@ class ListShine_Api extends ListShine_Api_Base
 {
     private $api_host;
     function __construct($api){
-        $this->api_host = "http://dev-mariop.listshine.com/api/v1/";
+        $this->api_host = "http://send.listshine.com/api/v1/";
         parent :: __construct($api, $this->api_host);
     }
 }
